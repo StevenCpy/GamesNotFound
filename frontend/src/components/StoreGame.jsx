2c4a8fecb94c00c5415511884e339828b4e1470c
StevenCPY <scpy21@gmail.com>
2026-02-20 07:45:14 -0500

    Merged frontend and backend into repo

--- frontend/src/components/StoreGame.jsx
+++ frontend/src/components/StoreGame.jsx
@@ -8,16 +8,16 @@ function StoreGame( {gameName, gameVersion, author, coverImageName} ) {
             gap:"1rem"
         }}>
             <div>
-                <img src={"/public/game-cover-images/" + coverImageName} alt={gameName} style={{ height:"10rem", width:"10rem" }} />
+                <img src={"/game-cover-images/" + coverImageName} alt={ gameName } style={{ height:"10rem", width:"10rem" }} />
             </div>
             <div style={{
                 height:"100%",
                 width:"100%"
             }}>
-                <div style={{ fontSize:"2.5rem", textAlign:"center" }}><b>{gameName}</b></div>
-                <div><b>VERSION: </b>{gameVersion}</div>
-                <div><b>AUTHOR: </b>{author}</div>
-                <button disabled>Install</button>
+                <div style={{ fontSize:"2.5rem", textAlign:"center" }}> <b>{ gameName }</b> </div>
+                <div> <b>VERSION: </b> { gameVersion } </div>
+                <div> <b>AUTHOR: </b> { author } </div>
+                <button disabled> Install </button>
             </div>
         </div>
     )
