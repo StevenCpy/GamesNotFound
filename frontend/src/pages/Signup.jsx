257036a7905875107686ff90bccddd12a8d4b621
StevenCPY <scpy21@gmail.com>
2026-02-25 06:17:56 -0500

    Added length check for username, email and password

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -5,6 +5,23 @@ function Signup() {
     const [username, setUsername] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [signedUp, setSignedUp] = useState(false)
+    
+    const usernameMaxLength = 30
+    const emailMaxLength = 255
+    const passwordMaxLength = 128
+
+    function handleField(e, setField, fieldMaxLength) {
+        if (e.target.value.length < fieldMaxLength) {
+            setField(e.target.value)
+        }
+    }
+
+    function handleSignUp() {
+        // TODO -- add backend logic for checking if email and username are available
+        // TODO -- add password validation, password must follow regex
+        setSignedUp(true)
+    }
 
     return (
         <form>
@@ -21,27 +38,29 @@ function Signup() {
                 <input
                     type="text"
                     value={username}
-                    onChange={(e) => setUsername(e.target.value)}
+                    onChange={(e) => handleField(e, setUsername, usernameMaxLength)}
                 />
                 <label>Email:</label>
                 <input
                     type="text"
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={(e) => handleField(e, setEmail, emailMaxLength)}
                 />
                 <label>Password:</label>
                 <input
                     type="text"
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={(e) => handleField(e, setPassword, passwordMaxLength)}
                 />
                 {/* show this text only if username, email or password is blank */}
                 {!(username && email && password) && <div style={{ color:"red" }} >Fill in all fields!</div>}
-                <button disabled={!(username && email && password)}>Sign up</button>
+                <button disabled={!(username && email && password)} onClick={ handleSignUp }>Sign up</button>
                 <span>
                     Already have an account?{" "}
                     <Link to="/Login">Login</Link>
                 </span>
+
+
 
             </div>
 
