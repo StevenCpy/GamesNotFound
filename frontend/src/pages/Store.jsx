4786ba4a928c4eef1332c2cf0ce0ebd9ff9efbb3
StevenCPY <scpy21@gmail.com>
2026-05-25 02:34:18 -0400

    Modify Store to use the games list obtained from server instead of hard-coded js

--- frontend/src/pages/Store.jsx
+++ frontend/src/pages/Store.jsx
@@ -1,14 +1,22 @@
+import { useContext } from 'react'
 import GameCard from '../components/GameCard'
 import games from '../data/games'
+import { AuthContext } from '../components/Context'
 
 function Store() {
+    const { store } = useContext(AuthContext)
+
     return (
         <div style={{
             display:"flex",
             flexDirection: "column",
         }}>
-            {games.games.map(game =>
-                <GameCard key={game.Id} gameName={game.gameName} gameVersion={game.gameVersion} author={game.author} coverImageName={game.coverImageName} />
+            {store.map(game =>
+                <GameCard key={game.gameID}
+                            gameID={game.gameID}
+                            gameName={game.gameName}
+                            gameVersion={`${game.gameMajor}.${game.gameMinor}.${game.gamePatch}`}
+                            author={game.author} />
             )}
         </div>
     )
