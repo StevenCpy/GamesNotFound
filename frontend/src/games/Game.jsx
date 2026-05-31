9dadeab69e57c519d487064c3bf7117f84f7d830
StevenCPY <scpy21@gmail.com>
2026-05-31 07:15:10 -0400

    Moved Game component styling to .css file

--- frontend/src/games/Game.jsx
+++ frontend/src/games/Game.jsx
@@ -1,21 +1,10 @@
+import "./Game.css"
+
 function Game({ gameName, game }) {
     return (
-        <div className="game-container"
-        style={{
-            flex: 1,
-            display: "flex",
-            flexDirection: "column",
-            fontSize: "5vh"
-        }}>
+        <div className="game-container">
             <div style={{ textAlign: "center" }}>Playing... {gameName}</div>
-            <div className="playable-area-container"
-            style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center"
-            }}>
+            <div className="playable-area-container">
                 {game}
             </div>
         </div>
