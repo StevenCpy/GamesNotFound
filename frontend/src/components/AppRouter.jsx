8cf9c7bf8f6617752d13a97d8ce954fb6851dc6f
StevenCPY <scpy21@gmail.com>
2026-05-31 02:51:14 -0400

    Fixed buggy CSS styles

--- frontend/src/components/AppRouter.jsx
+++ frontend/src/components/AppRouter.jsx
@@ -19,19 +19,28 @@ function AppRouter() {
 
     return (
         <BrowserRouter>
-            <NavbarMain />
+            <div id="app-container"
+            style={{
+                width: "100vw",
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column"
+            }}>
+                <NavbarMain />
 
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/Store" element={<Store />} />
-                <Route path="/Library" element={currentUser ? <Library /> : <RestrictedResource />} />
-                <Route path="/Profile" element={<Profile />} />
-                <Route path="/Signup" element={<Signup />} />
-                <Route path="/Login" element={<Login />} />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/Store" element={<Store />} />
+                    <Route path="/Library" element={currentUser ? <Library /> : <RestrictedResource />} />
+                    <Route path="/Profile" element={<Profile />} />
+                    <Route path="/Signup" element={<Signup />} />
+                    <Route path="/Login" element={<Login />} />
 
-                <Route path="*" element={<Error404 />} /> {/* Error page for invalid URLs */}
-                {import.meta.env.DEV && <Route path="/games/HitTheTarget" element={<HitTheTarget />} />}
-            </Routes>
+                    <Route path="*" element={<Error404 />} /> {/* Error page for invalid URLs */}
+                    {import.meta.env.DEV && <Route path="/games/HitTheTarget" element={<HitTheTarget />} />}
+                </Routes>
+            </div>
+            
         </BrowserRouter>
     )
 }
