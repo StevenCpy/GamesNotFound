ffed7348d8b4fcff2f88253f1d9cd15ee3a66f09
StevenCPY <scpy21@gmail.com>
2026-06-02 21:57:46 -0400

    Added timer to game using setInterval()

--- frontend/src/games/Hit the Target/Hit the Target.jsx
+++ frontend/src/games/Hit the Target/Hit the Target.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect, useRef, createContext, useContext } from "react"
 import './Hit the Target.css'
 
+const START_TIME_S = 5
+
 const ScoreContext = createContext(null)
+const GameStatusContext = createContext(null)
 
 function Target({ playableSize, onTargetHit }) {
     const [targetSize, setTargetSize] = useState({width: 0, height: 0})
@@ -34,11 +37,31 @@ function Target({ playableSize, onTargetHit }) {
     )
 }
 
+function Timer() {
+    const [timeSeconds, setTimeSeconds] = useState(START_TIME_S)
+    const { isGameOn, setIsGameOn, isGameOver, setIsGameOver } = useContext(GameStatusContext)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimeSeconds(prev => prev-1)
+        }, 1000)
+
+        return () => clearInterval(interval)
+    }, [])
+
+    return (
+        <>
+            Timer: {timeSeconds}s
+        </>
+    )
+}
+
 function HittheTarget() {
     const [score, setScore] = useState(0)
     const [playableSize, setPlayableSize] = useState({width: 0, height: 0})
     const [refresh, setRefresh] = useState(0)
     const [isGameOn, setIsGameOn] = useState(false)
+    const [isGameOver, setIsGameOver] = useState(false)
 
     const playableAreaRef = useRef(null)
 
@@ -47,15 +70,20 @@ function HittheTarget() {
     }, [refresh])
 
     return (
-        <ScoreContext value={{ setScore }}>
-            <div id="playable-area" ref={playableAreaRef}>
-                <span style={{ color: "black" }}>Score: {score}</span>
-                {isGameOn ? 
-                    <Target playableSize={playableSize} onTargetHit={() => setRefresh(refresh+1)} />
-                    : <div id="start-button-container"><button id="start-button" onClick={() => setIsGameOn(true)}>START</button></div>
-                }
-            </div>
-        </ScoreContext>
+        <GameStatusContext value={{ isGameOn, setIsGameOn, isGameOver, setIsGameOver }}>
+            <ScoreContext value={{ setScore }}>
+                <div id="playable-area" ref={playableAreaRef}>
+                    <div id="score-timer-container">
+                        <span>Score: {score}</span>
+                        <span><Timer onGameOver/></span>
+                    </div>
+                    {isGameOn ? 
+                        <Target playableSize={playableSize} onTargetHit={() => setRefresh(refresh+1)} />
+                        : <div id="start-button-container"><button id="start-button" onClick={() => setIsGameOn(true)}>START</button></div>
+                    }
+                </div>
+            </ScoreContext>
+        </GameStatusContext>
     )
 }
 
