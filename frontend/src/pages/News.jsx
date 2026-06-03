c5be58835cb5387f263738c39006780769bf8d0e
StevenCPY <scpy21@gmail.com>
2026-06-03 08:52:26 -0400

    Added changelogs to News page

--- frontend/src/pages/News.jsx
+++ frontend/src/pages/News.jsx
@@ -1,10 +1,34 @@
 import { Link } from "react-router-dom"
 
+import './styling/News.css'
+
 function News() {
     return (
-        <div>
+        <div id="news-page">
             <h1> What's NEW! </h1>
-            - Created game called "Hit the Target".  <Link style={{ color: "red" }} to="/games/HitTheTarget">Quick link to game</Link>
+            <ul className="ul-dash">
+                <li>
+                    Created the "What's NEW" page to share new features and changelogs."
+                </li>
+                <li>
+                    Created first game called "Hit the Target".{" "}
+                    <Link style={{ color: "green" }} to="/games/HitTheTarget">Quick link to game</Link>
+                </li>
+                <li>
+                    Improved site layout.<br />
+                    <div id="layout-change">
+                        <span>
+                            Old layout:<br />
+                            <img src={`/news-images/Old layout.png`} alt="Old layout"></img>
+                        </span>
+                        <span>
+                            New layout:<br />
+                            <img src={`/news-images/New layout.png`} alt="New layout"></img>
+                        </span>
+                    </div>
+                    
+                </li>
+            </ul>
             
         </div>
     )
