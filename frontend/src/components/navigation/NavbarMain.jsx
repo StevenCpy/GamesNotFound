4da1a4ecc69cfc71d3d85b3b689e3c77197554d5
StevenCPY <scpy21@gmail.com>
2026-06-03 05:13:15 -0400

    Fixed CSS styling for main navbar

--- frontend/src/components/navigation/NavbarMain.jsx
+++ frontend/src/components/navigation/NavbarMain.jsx
@@ -1,38 +1,28 @@
 import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+
 import { AuthContext } from "../contexts/AuthContext"
+import './NavbarMain.css'
 
 function NavbarMain() {
     const { currentUser, setCurrentUser } = useContext(AuthContext)
 
     return (
-        <nav id="main-nav-bar">
-            <div style={{
-                display: "flex",
-                flexDirection: "row",
-            }}>
-                <span style={{
-                    display: "flex",
-                    gap: "30px"
-                }}>
-                    <Link to="/">GamesNotFound</Link>
-                    <Link to="/store">STORE</Link>
-                    {currentUser && <Link to="/library">LIBRARY</Link>}
-                    <Link to="/news">What's NEW</Link>
-                </span>
+        <nav id="main-navbar">
+            <span id="main-navbar-left">
+                <Link to="/">GamesNotFound</Link>
+                <Link to="/store">STORE</Link>
+                {currentUser && <Link to="/library">LIBRARY</Link>}
+                <Link to="/news">What's NEW</Link>
+            </span>
 
-                <span style={{
-                    marginLeft: "auto",
-                    display: "flex",
-                    gap: "30px"
-                }}>
-                    {import.meta.env.DEV && <Link to="/games/HitTheTarget">LINK FOR TESTING</Link>}
-                    {(!currentUser && import.meta.env.DEV) && <button onClick={() => setCurrentUser("Admin")}>Quick Login</button>}
-                    {currentUser ?
-                        <Link to="/profile">{currentUser}</Link> : <Link to="/login">Login</Link>
-                    }
-                </span>
-            </div>
+            <span id="main-navbar-right">
+                {import.meta.env.DEV && <Link to="/games/HitTheTarget">LINK FOR TESTING</Link>}
+                {(!currentUser && import.meta.env.DEV) && <button onClick={() => setCurrentUser("Admin")}>Quick Login</button>}
+                {currentUser ?
+                    <Link to="/profile">{currentUser}</Link> : <Link to="/login">Login</Link>
+                }
+            </span>
         </nav>
     )
 }
