30b47df6b964634f8ec9132ec9b1598543000939
StevenCpy <96119669+StevenCpy@users.noreply.github.com>
2026-06-06 02:41:22 -0400

    Fix/incorrect routes store and library (#157)
    * Fixed incorrect routes for /store/ and /library/
    
    * Small fix - Added type to params on backend

--- backend/routes/v2/utils.py
+++ backend/routes/v2/utils.py
@@ -4,11 +4,11 @@ import jwt
 JWT_HS256_SECRET_KEY = os.environ.get("JWT_HS256_SECRET_KEY")
 
 # JWT
-def encode_HS256(payload):
+def encode_HS256(payload: dict) -> str:
     payload_encoded = jwt.encode(payload, JWT_HS256_SECRET_KEY, algorithm="HS256")
     return payload_encoded
 
-def decode_payload_HS256(Authorization):
+def decode_payload_HS256(Authorization: str|None = None):
     token = Authorization.replace("Bearer ", "")
     payload_decoded = jwt.decode(token, JWT_HS256_SECRET_KEY, algorithms="HS256")
     return payload_decoded
--- backend/utils/logging/logging.py
+++ backend/utils/logging/logging.py
@@ -1,10 +1,10 @@
 import sys
 
-def dev_log(func, message):
+def dev_log(func: str, message: str):
     print(f"{func} - {message}")
 
-def dev_error(func, error_message):
+def dev_error(func: str, error_message: str):
     print(f"{func} - {error_message}", file=sys.stderr)
 
-def dev_error_database(func, error_message):
+def dev_error_database(func: str, error_message: str):
     print(f"{func} - Database error: {error_message}", file=sys.stderr)
