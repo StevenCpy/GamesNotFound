8bae5c96400256ec06cd2ea67f6a47b029b52235
StevenCPY <scpy21@gmail.com>
2026-08-10 18:20:55 -0400

    Added caching mechanism on server for Redis-based leaderboard

--- backend/routes/v3/leaderboard.py
+++ backend/routes/v3/leaderboard.py
@@ -1,5 +1,5 @@
 # fastAPI
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 # supabase
 from ..supabase_client import supabase_client, SCORE_TABLE
@@ -8,6 +8,10 @@ from ..supabase_client import supabase_client, SCORE_TABLE
 from ..status_message import status_success, status_fail
 from utils.logging import dev_log, dev_error, dev_error_database
 
+import redis.asyncio as redis
+from ..redis_client import get_redis_client
+import json
+
 router = APIRouter(
     prefix="/leaderboard"
 )
@@ -15,11 +19,18 @@ router = APIRouter(
 # ----------------------------------------------------------------- #
 #                      /leaderboard/{gameID}                        #
 # ----------------------------------------------------------------- #
-# API to return games high scores
+# API to return leaderboard for gameID
 @router.get("/{gameID}")
-async def show_leaderboard(gameID: int):
+async def show_leaderboard(gameID: int, redis_client: redis.Redis = Depends(get_redis_client)):
     endpoint = "leaderboard"
     dev_log(endpoint, "Endpoint called")
+
+    # check Redis cache
+    cache_key = f"/leaderboard/{gameID}"
+    cache_value = await redis_client.get(cache_key)
+
+    if cache_value:
+        return status_success(json.loads(cache_value))
 
     # query database for TOP 5 high scores
     try:
@@ -32,6 +43,8 @@ async def show_leaderboard(gameID: int):
             .execute()
         )
         dev_log(endpoint, f"Top 5 High scores for '{gameID}' fetched from database")
+        exp_time_s = 60*60
+        await redis_client.setex(cache_key, exp_time_s, json.dumps(response.data)) # set leaderboard in cache with TTL of 1 hour
         return status_success(response.data)
     except Exception as e:
         dev_error_database(endpoint, e)
